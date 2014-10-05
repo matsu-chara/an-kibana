09053f7ebe954f8606a56fc8abd338f743f0b1d1
matsu-chara <matsuy00@gmail.com>
2014-10-06 01:45:17 +0900

    use gem module for fluent plugin

--- spec/default/fluentd_spec.rb
+++ spec/default/fluentd_spec.rb
@@ -8,3 +8,8 @@ describe service('td-agent') do
   it { should be_enabled   }
   it { should be_running   }
 end
+
+describe command('/usr/lib64/fluent/ruby/bin/fluent-gem list') do
+    it { should return_exit_status 0 }
+    it { should return_stdout(/fluent-plugin-elasticsearch/) }
+end
